ebbe31b9665d71946dcb9412e96332268fc91bbc
Meibin Lee <kikittong@naver.com>
2020-08-22 16:33:49 +0900

    project initiated

---
+++ tripdiary/src/main/java/ac/hongik/tripdiary/TripdiaryApplication.java
@@ -0,0 +1,13 @@
+package ac.hongik.tripdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripdiaryApplication.class, args);
+	}
+
+}
---
+++ tripdiary/src/test/java/ac/hongik/tripdiary/TripdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.hongik.tripdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
